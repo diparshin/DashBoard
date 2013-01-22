c912e905ff3408e281069bf5867d7ab64ddaf0fb
diparshin <d.i.parshin@gmail.com>
2013-01-23 00:40:13 +0400

    Add redirect to manifestly specified locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :set_locale
+  before_filter :set_locale  
   before_filter :set_locale_from_url
+  before_filter :redirect_to_default_client_locale
   
   private
+  
+  def redirect_to_default_client_locale
+    unless params[:locale]
+      redirect_to '/' + I18n.locale.to_s + url_for(:only_path => true)
+    end
+  end
   
   def set_locale
     I18n.locale = params[:locale] || ((lang = request.env['HTTP_ACCEPT_LANGUAGE']) && lang[/^[a-z]{2}/])
